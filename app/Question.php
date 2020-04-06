5d4df95d118d41a48582c788c9cb0d33f568e6f0
vikitaotiz <victorotieno598@gmail.com>
2020-04-06 22:21:29 +0300

    Added Relationship

--- app/Question.php
+++ app/Question.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
